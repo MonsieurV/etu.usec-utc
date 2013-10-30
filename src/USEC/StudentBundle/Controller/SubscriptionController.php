b197ccfcaff5e47c5c70e967785a63866992fd5e
MonsieurV <Monsieur@server.fake>
2013-10-29 18:52:49 -0600

    Add license headers to project files

--- src/USEC/StudentBundle/Controller/SubscriptionController.php
+++ src/USEC/StudentBundle/Controller/SubscriptionController.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Plateforme étudiante USEC.
+*
+* (c) USEC <contact@usec-utc.fr>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace USEC\StudentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
--- src/USEC/StudentBundle/Entity/Student.php
+++ src/USEC/StudentBundle/Entity/Student.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the Plateforme étudiante USEC.
+*
+* (c) USEC <contact@usec-utc.fr>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace USEC\StudentBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
